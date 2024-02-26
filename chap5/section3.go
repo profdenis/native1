ea603febe2e98ae310d56ef7e5febc16fd5890f2
Denis Rinfret <denis.rinfret@cegepdrummond.ca>
2024-02-26 16:13:06 -0500

    mise à jour chap 5 section 3

--- chap5/section3.go
+++ chap5/section3.go
@@ -3,7 +3,9 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
+	"strings"
 )
 
 type Person struct {
@@ -11,11 +13,15 @@ type Person struct {
 	Age  int    `json:"age"`
 }
 
+//func (p Person) String() string {
+//	return fmt.Sprintf("Person(%s, %d)", p.Name, p.Age)
+//}
+
 func section3a() {
 	// Créer une instance de Person
 	p := Person{Name: "John", Age: 30}
 
-	// Séraliser la struct en JSON
+	// Sérialiser la struct en JSON
 	jsonData, err := json.Marshal(p)
 	if err != nil {
 		fmt.Println(err)
@@ -36,7 +42,7 @@ func section3a() {
 
 func section3b() {
 	// Créer une instance de Person
-	p := Person{Name: "John", Age: 30}
+	p := Person{Name: "Denis", Age: 30}
 
 	// Sérialiser la struct en JSON
 	jsonData, err := json.Marshal(p)
@@ -46,7 +52,7 @@ func section3b() {
 	}
 
 	// Ouvrir le fichier en mode écriture
-	file, err := os.OpenFile("person.json", os.O_CREATE|os.O_WRONLY, 0644)
+	file, err := os.OpenFile("person.json", os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
 	if err != nil {
 		fmt.Println(err)
 		return
@@ -88,7 +94,7 @@ func section3c() {
 		{Name: "Alice", Age: 35},
 	}
 
-	// Séraliser la slice en JSON
+	// Sérialiser la slice en JSON
 	jsonData, err := json.MarshalIndent(people, "", "\t")
 	if err != nil {
 		fmt.Println(err)
@@ -128,4 +134,74 @@ func section3c() {
 
 	// Afficher la slice de Person
 	fmt.Println(people2) // Devrait afficher : [{John 30} {Jane 25} {Alice 35}]
+}
+
+type People []Person
+
+func (people People) String() string {
+	var builder strings.Builder
+	for i, p := range people {
+		builder.WriteString(fmt.Sprintf("%d: %v\n", i, p))
+	}
+
+	return builder.String()
+}
+
+func section3d() {
+	// Créer une tranche de Person
+	people := People{
+		{Name: "John", Age: 30},
+		{Name: "Jane", Age: 25},
+		{Name: "Alice", Age: 35},
+	}
+
+	err := WritePeople(people, "fichiers/people.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ReadPeople(&people, "fichiers/people.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println(people)
+}
+
+func ReadPeople(people *People, filename string) error {
+	// Lire les données du fichier
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		return err
+	}
+
+	// Désérialiser les données JSON pour recréer une tranche de Person
+	err = json.Unmarshal(data, &people)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func WritePeople(people People, filename string) error {
+	// Sérialiser la slice en JSON
+	jsonData, err := json.MarshalIndent(people, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	// Ouvrir le fichier en mode écriture
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	// Écrire les données JSON dans le fichier
+	_, err = file.Write(jsonData)
+	if err != nil {
+		return err
+	}
+	return nil
 }
