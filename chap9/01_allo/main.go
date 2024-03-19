e796575f8cba8f367789c9a73e61ae1a2f0f6a32
Denis Rinfret <denis.rinfret@cegepdrummond.ca>
2024-03-19 13:12:10 -0400

    ajout d'un lien aud code fyne

--- chap9/01_allo/main.go
+++ chap9/01_allo/main.go
@@ -2,22 +2,24 @@ package main
 
 import (
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 )
 
 // exemples basés sur la doc officielle : https://docs.fyne.io
+// code du projet fyne : https://github.com/fyne-io/fyne
 
 func main() {
 	a := app.New()
 	w := a.NewWindow("Allo")
 
 	allo := widget.NewLabel("Allo")
-	w.SetContent(allo)
-	//w.SetContent(container.NewVBox(
-	//	allo,
-	//	widget.NewButton("Salut !", func() {
-	//		allo.SetText("Bienvenue 😀")
-	//	}),
-	//))
+	//w.SetContent(allo)
+	w.SetContent(container.NewVBox(
+		allo,
+		widget.NewButton("Salut !", func() {
+			allo.SetText("Bienvenue 😀")
+		}),
+	))
 	w.ShowAndRun()
 }
